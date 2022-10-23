1290730de4f3187329145248f499a3962cc6bf6a
uidops <uidops@protonmail.com>
2022-10-23 20:38:26 +0330

    add _rest_args in ARG_START

--- include/arg.h
+++ include/arg.h
@@ -1,13 +1,20 @@
 #ifndef ARG_H
 #define ARG_H
 
-#define ARG_START for (int i = 1; i < argc; i++) { \
-	if (argv[i][0] == '-') { \
-		char _arg = argv[i][1]; \
-		switch (argv[i][1]) {
+#define ARG_START size_t _n = 0; \
+	char **_rest_args = calloc(argc, sizeof(char *)); \
+	for (int i = 1; i < argc; i++) { \
+		if (argv[i][0] != '-' && argv[i-1][0] != '-') { \
+			_rest_args[_n++] = argv[i]; \
+			continue; \
+		} else { \
+			char _arg = argv[i][1]; \
+			switch (argv[i][1]) {
 
 #define ARG_END } \
 	} \
 }
+
+#define ARG_REST_FREE free(_rest_args);
 
 #endif
