9f22923dfee2cafaa0724e961ceeb4412c9b5b6c
uidops <uidops@protonmail.com>
2022-10-23 23:44:05 +0330

    rewrite the 'ignore '-' character in ARG_START'

--- include/arg.h
+++ include/arg.h
@@ -4,10 +4,10 @@
 #define ARG_START size_t _n = 0; \
 	char **_rest_args = calloc(argc, sizeof(char *)); \
 	for (int i = 1; i < argc; i++) { \
-		if (argv[i][0] != '-' && argv[i-1][0] != '-') { \
+		if ((argv[i][0] != '-' && argv[i-1][0] != '-') || (argv[i][0] == '-' && argv[i][1] == 0x00)) { \
 			_rest_args[_n++] = argv[i]; \
 			continue; \
-		} else if (argv[i][0] == '-' && argv[i][1] != 0) { \
+		} else if (argv[i][0] == '-') { \
 			char *_arg = argv[i+1]; \
 			switch (argv[i][1]) {
 
