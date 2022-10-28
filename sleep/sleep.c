617e64c7bf0fdeb8b4c6f8199fd8335ab60eebe8
uidops <uidops@protonmail.com>
2022-10-29 00:56:07 +0330

    add sleep and its makefile

---
+++ sleep/sleep.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "arg.h"
+
+void		 usage(char *__progname);
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 2)
+		usage(*argv);
+
+	ARG_START
+		default:
+			usage(*argv);
+			break;
+	ARG_END
+
+	char **args = _rest_args;
+	for (; *args != NULL; args++) {
+		unsigned int num = atoi(*(argv+1));
+		while ((num = sleep(num)));
+	}
+
+	ARG_REST_FREE
+
+	return EXIT_SUCCESS;
+}
+
+
+void
+usage(char *__progname)
+{
+	printf("usage: %s [num ...]\n", __progname);
+	exit(EXIT_FAILURE);
+}
